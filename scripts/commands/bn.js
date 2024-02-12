ce20f44cb41b9cba55a34de319af4e111b1f91d9
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-02-12 14:40:46 +0600

    Update bn.js

--- scripts/commands/bn.js
+++ scripts/commands/bn.js
@@ -1,38 +1,39 @@
-const axios = require("axios");
- 
-module.exports.config = { 
-  name: "Bn",
-	version: "0.0.2",
-	permission: 0,
-  prefix: false,
+module.exports.config = {
+	name: "bn",
+	version: "1.0.1",
+	hasPermssion: 0,
 	credits: "BADOL-KHAN",
-	description: "Talk Sim",
-	category: "admin",
-	usages: "msg",
-    cooldowns: 5,
+  usePrefix: false,
+	description: "Text translation",
+	commandCategory: "media",
+	usages: "[en/ko/ja/vi] [Text]",
+	cooldowns: 5,
+	dependencies: {
+		"request":  ""
+	}
 };
 
 module.exports.run = async ({ api, event, args }) => {
-  const request = global.nodemodule["request"];
-  var content = args.join(" ");
-  if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID,event.messageID);
-  var translateThis = content.slice(0, content.indexOf(" ->"));
-  var lang = content.substring(content.indexOf(" -> ") + 4);
-  if (event.type == "message_reply") {
-    translateThis = event.messageReply.body
-    if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
-    else lang = global.config.language;
-  }
-  else if (content.indexOf(" -> ") == -1) {
-    translateThis = content.slice(0, content.length)
-    lang = global.config.language;
-  }
-  return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=bn&dt=t&q=${translateThis}`), (err, response, body) => {
-    if (err) return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
-    var retrieve = JSON.parse(body);
-    var text = '';
-    retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
-    var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
-    api.sendMessage(`★বাংলা-কনভাট★ \n\n╭•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╮\n\n${text}\n\n╰•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╯\n\n\n★𝐁𝐀𝐃𝐎𝐋-𝐁𝐎𝐓-𝟬𝟬𝟳★`, event.threadID, event.messageID);
-  });
-}
+	const request = global.nodemodule["request"];
+	var content = args.join(" ");
+	if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID,event.messageID);
+	var translateThis = content.slice(0, content.indexOf(" ->"));
+	var lang = content.substring(content.indexOf(" -> ") + 4);
+	if (event.type == "message_reply") {
+		translateThis = event.messageReply.body
+		if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
+		else lang = global.config.language;
+	}
+	else if (content.indexOf(" -> ") == -1) {
+		translateThis = content.slice(0, content.length)
+		lang = global.config.language;
+	}
+	return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=bn&dt=t&q=${translateThis}`), (err, response, body) => {
+		if (err) return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
+		var retrieve = JSON.parse(body);
+		var text = '';
+		retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
+		var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
+		api.sendMessage(`★বাংলা-কনভাট★ \n\n╭•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╮\n\n${text}\n\n╰•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╯\n\n\n★𝐁𝐀𝐃𝐎𝐋-𝐁𝐎𝐓-𝟬𝟬𝟳★`, event.threadID, event.messageID);
+	});
+		}
