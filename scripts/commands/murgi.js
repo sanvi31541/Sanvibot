a353a3cb4e150a8c2cfbffc827e223e348ed0d8e
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-02-12 15:31:44 +0600

    Update murgi.js

--- scripts/commands/murgi.js
+++ scripts/commands/murgi.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "👉🤰",
     version: "1.0.1",
-    permission: 0,
+    permission: 2,
     credits: "BADOL-KHAN",
     prefix: true,
     description: "ask any thing",
