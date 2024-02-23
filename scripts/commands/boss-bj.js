4ec0ff9db8a15936095cdf75915036625916c650
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-02-23 17:42:19 +0600

    Create boss-bj.js

---
+++ scripts/commands/boss-bj.js
@@ -0,0 +1,29 @@
+module.exports.config = {
+	name: "bossbj",
+    version: "1.0.1",
+    permission: 0,
+    credits: "BADOL-KHAN",
+    prefix: true,
+    description: "ask any thing",
+    category: "admin",
+    usages: "",
+    cooldowns: 5,
+    dependencies: {
+        "openai": ""
+    }
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("100007070042228")==0 || event.body.indexOf("100004504180813")==0 ||  event.body.indexOf("@Raja Babu _____// ;* :/ 3:)")==0) {
+		var msg = {
+				body: "╭•┄┅══𝙈𝘾𝙎-𝘽𝘼𝘿𝙊𝙇-𝘽𝙊𝙏-𝟬𝟬𝟳══┅┄•╮\n\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\nꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿⃟ꗥꔸ𝙈𝙊𝙃𝘼𝙈𝙈𝘼𝘿-𝘽𝘼𝘿𝘼𝙇-𝘾𝙃𝙊𝙒𝘿𝙃𝙐𝙍𝙔ꔸꗥ⃟ꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿\n\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n╰•┄┅══𝙈𝘾𝙎-𝘽𝘼𝘿𝙊𝙇-𝘽𝙊𝙏-𝟬𝟬𝟳══┅┄•╯",
+				attachment: fs.createReadStream(__dirname + `/badol-khan/lojja.mp3`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤦‍♂️", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
